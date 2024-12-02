5f691d53d519d59d2d0342779490b6cac1b6ee51
Daniel Idoko <idokoidogwu@gmail.com>
2024-12-02 01:37:28 +0100

    Initializing project on git

---
+++ bin/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ==============================================================================
+# SCRIPT:       main.sh
+# AUTHOR:       Idogwu
+# CREATED:      2nd December, 2024
+# UPDATED:      N/A
+# VERSION:      1.0
+# DESCRIPTION:  This is a handy man script that makes the basic tasks of the linux system easy for sysadmins
+# USAGE:        ./main.sh [options]
+# LICENSE:      [Your License Information]
+# ==============================================================================
